45ad850c9111a7c09185b141e6d7540e3c2e7d65
Andrew James Collett <ajcollett.sun@gmail.com>
2016-05-17 09:48:39 +0200

    Cleaned things up some more

--- pyManager.py
+++ pyManager.py
@@ -1,8 +1,6 @@
-# This code is free to use! It comes with no Warrenties however.
-# If you want to help make it better, please let me know about
-# any changes.
-
-# This is intended to help you pull your JSON objects from manager.
+# This code is free to use! However, it comes with no warrenties.
+# Please let me know about any improvements
+# Repo at https://github.com/ajcollett/pyManager
 
 """API calling script for Manager.io."""
 import requests
@@ -20,22 +18,21 @@ __copyright__ = 'Copyright 2016 Andrew James Collett'
 class manager_object:
     """Represents the info from the the manager.io."""
 
-    def __init__(self, host, user, business):
+    def __init__(self, root_url, user, business):
         """Create the Manager object.
 
         Start the session, get the businss and the index of collections
         """
-        self.host = host
+        self.root_url = root_url
         self.user = user
         self.session = requests.Session()
         self.session.auth = (user, getpass.getpass())
         self.business = self.get_business(business)
         self.index = self.get_index_of_collections()
 
-    # This is the base method to get the url
     def get_encodedURL(self, requestURL):
-        """Fetch the URL presumably HTTPS."""
-        r = self.session.get('https://' + self.host + requestURL)
+        """Fetch the URL."""
+        r = self.session.get(self.root_url + requestURL)
         return r
 
     def get_business(self, business):
@@ -69,21 +66,26 @@ class manager_object:
                                                   ).json()
         return objects
 
-    def put_object(self, object_path):
+    def put_object(self, data, object_path):
         """Put an object at that specific path."""
-        # Not yer implemented
+        print('Not yet implemented')
         pass
 
-    def post_object(self, object_path):
+    def post_object(self, data, object_path):
         """Post an object at that specific path."""
-        # Not yer implemented
+        print('Not yet implemented')
+        pass
+
+    def del_object(self, object):
+        """Delete the specified object."""
+        print('Not yet implemented')
         pass
 
 
 def define_args():
     """We define the arguents here for the command line."""
     parser = argparse.ArgumentParser(description='API calls for manager')
-    parser.add_argument('host')
+    parser.add_argument('root_url')
     parser.add_argument('user')
     parser.add_argument('business')
     parser.add_argument('object')
@@ -95,10 +97,10 @@ def __main__():
     parser = define_args()
     args = parser.parse_args()
 
-    gom = manager_object(args.host, args.user, args.business)
+    gom = manager_object(args.root_url, args.user, args.business)
     object_index = gom.get_index_of_objects(gom.index[args.object])
-    print object_index
-    print gom.get_objects_from_index(object_index)
+    print(object_index)
+    print(gom.get_objects_from_index(object_index))
 
 if __name__ == '__main__':
     __main__()

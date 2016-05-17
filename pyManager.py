58bee32c28a0b06d81e37fe02df64479b31adb58
Andrew James Collett <ajcollett.sun@gmail.com>
2016-05-17 09:58:37 +0200

    Renamed things and generally neatened things up

--- pyManager.py
+++ pyManager.py
@@ -3,39 +3,51 @@
 # any changes.
 
 # This is intended to help you pull your JSON objects from manager.
+
+"""API calling script for Manager.io."""
 import requests
 import argparse
 import getpass
 from BeautifulSoup import BeautifulSoup
 
 __author__ = 'Andrew James Collett'
+__title__ = 'pyManager'
+__email__ = 'andrewjamescollett@gmail.com'
+__license__ = 'LGPL3'
+__copyright__ = 'Copyright 2016 Andrew James Collett'
 
 
-class manager_objects:
+class manager_object:
+    """Represents the info from the the manager.io."""
 
     def __init__(self, host, user, business):
+        """Create the Manager object.
+
+        Start the session, get the businss and the index of collections
+        """
         self.host = host
         self.user = user
         self.session = requests.Session()
-        self.session.auth(user, getpass.getpass())
+        self.session.auth = (user, getpass.getpass())
         self.business = self.get_business(business)
         self.index = self.get_index_of_collections()
 
     # This is the base method to get the url
     def get_encodedURL(self, requestURL):
+        """Fetch the URL presumably HTTPS."""
         r = self.session.get('https://' + self.host + requestURL)
         return r
 
-    # Get the business
     def get_business(self, business):
+        """Fetch the path to the specific business."""
         json_index = self.get_encodedURL('/api/index.json').json()
         for pair in json_index:
             if pair['Name'] == business:
                 return pair['Key']
         return None
 
-    # Get the index for the business collections
     def get_index_of_collections(self):
+        """Fetch the paths to the collection."""
         index = dict()
         r = self.get_encodedURL('/api/' + self.business)
         soup = BeautifulSoup(r.text)
@@ -44,11 +56,12 @@ class manager_objects:
 
         return index
 
-    # Get the index for the collection
     def get_index_of_objects(self, collection_path):
+        """Fetch all the paths to the objects."""
         return self.get_encodedURL(collection_path + '/index.json').json()
 
     def get_objects_from_index(self, object_index):
+        """Fetch each object from manager."""
         objects = dict()
         for object in object_index:
             objects[object] = self.get_encodedURL('/api/' + self.business +
@@ -56,9 +69,19 @@ class manager_objects:
                                                   ).json()
         return objects
 
+    def put_object(self, object_path):
+        """Put an object at that specific path."""
+        # Not yer implemented
+        pass
 
-# Create the arguments parser
+    def post_object(self, object_path):
+        """Post an object at that specific path."""
+        # Not yer implemented
+        pass
+
+
 def define_args():
+    """We define the arguents here for the command line."""
     parser = argparse.ArgumentParser(description='API calls for manager')
     parser.add_argument('host')
     parser.add_argument('user')
@@ -67,13 +90,12 @@ def define_args():
     return parser
 
 
-# Do the main thing
 def __main__():
-
+    """For the CLI, at the moment a test runner."""
     parser = define_args()
     args = parser.parse_args()
 
-    gom = manager_objects(args.host, args.user, args.business)
+    gom = manager_object(args.host, args.user, args.business)
     object_index = gom.get_index_of_objects(gom.index[args.object])
     print object_index
     print gom.get_objects_from_index(object_index)

3be87a4d08eb12cc58519bbc302f14c818fb7342
Andrew James Collett <ajcollett.sun@gmail.com>
2016-06-01 20:02:39 +0200

    Added some more info and python3 compatability

--- pyManager.py
+++ pyManager.py
@@ -1,6 +1,7 @@
 # This code is free to use! However, it comes with no warrenties.
 # Please let me know about any improvements
 # Repo at https://github.com/ajcollett/pyManager
+# Requires python 2.7.11 or up
 
 """API calling script for Manager.io."""
 import requests
@@ -8,8 +9,7 @@ import argparse
 import getpass
 import json
 from threading import Thread
-from BeautifulSoup import BeautifulSoup
-
+from bs4 import BeautifulSoup
 __author__ = 'Andrew James Collett'
 __title__ = 'pyManager'
 __email__ = 'andrewjamescollett@gmail.com'
@@ -30,54 +30,68 @@ class manager_object:
         self.session = requests.Session()
         self.session.auth = (user, getpass.getpass())
         self.business = self.get_business(business)
-        self.index = self.index_collections()
+        self.collections = self.index_collections()
 
-    def get_encodedURL(self, requestURL):
+    def get(self, requestURL):
         """Fetch the URL."""
         return self.session.get(self.root_url + requestURL)
 
-    def post_encodedURL(self, data, collection_path):
-        """Post to the URL."""
-        return self.session.post(self.root_url + collection_path, data=data)
-
     def get_business(self, business):
         """Fetch the path to the specific business."""
-        json_index = self.get_encodedURL('/api/index.json').json()
+        json_index = self.get('/api/index.json').json()
         for pair in json_index:
             if pair['Name'] == business:
                 return pair['Key']
         return None
 
     def index_collections(self):
-        """Fetch the paths to the collection."""
-        index = dict()
-        r = self.get_encodedURL('/api/' + self.business)
-        soup = BeautifulSoup(r.text)
+        """Fetch the paths to the collections."""
+        collections = dict()
+        r = self.get('/api/' + self.business)
+        soup = BeautifulSoup(r.text, "html.parser")
         for link in soup.findAll('a'):
-            index[link.getText()] = link.get('href')
+            collections[link.getText()] = link.get('href')
 
-        return index
+        return collections
 
 # Below, the indexing of objects, within a collection
     def index_objects(self, collection):
         """Fetch all the paths to the objects in a collection."""
-        return self.get_encodedURL(self.index[collection] +
-                                   '/index.json').json()
+        return self.get(self.collections[collection] + '/index.json').json()
 
-    def index_SaleInvoice(self):
+    def index_sales_invoices(self):
         """Fetch all the SalesInvoices."""
         return self.index_objects('SalesInvoice')
 
-    def index_Customer(self):
+    def index_customers(self):
         """Fetch all the Customer indexes."""
         return self.index_objects('Customer')
+
+    def index_cash_accounts(self):
+        """Fetch all the cash account indexes."""
+        return self.index_objects('CashAccount2')
+
+    def index_tracking_codes(self):
+        """Fetch all the Tracking codes indexes."""
+        return self.index_objects('TrackingCode')
+
+    def index_profit_loss_account(self):
+        """Fetch all the profit and loss accounts indexes."""
+        return self.index_objects('ProfitAndLossStatementAccount')
+
+    def index_sales_inventory_items(self):
+        """Fetch all the sales invoice items indexes."""
+        return self.index_objects('SalesInvoiceItem')
+
+    def index_inventory(self):
+        """Fetch all the inventory indexes."""
+        return self.index_objects('InventoryItem')
 
 # Below, fetching objects from a collection
     def get_object_thread(self, o_dict, index):
         """A thread function to make fetching objects faster."""
-        o_dict[index] = self.get_encodedURL('/api/' + self.business +
-                                            '/' + index +
-                                            '.json').json()
+        o_dict[index] = self.get('/api/' + self.business + '/' + index +
+                                 '.json').json()
 
     def get_objects(self, object_index):
         """
@@ -95,12 +109,7 @@ class manager_object:
                                      args=(objects, object))
             cnt = cnt + 1
             threads[object].start()
-            """
-            objects[object] = self.get_encodedURL('/api/' + self.business +
-                                                   '/' + object + '.json'
-                                                   ).json()
-                                                """
-            if cnt == 150:
+            if cnt == 100:
                 for thread in threads:
                     threads[thread].join()
                 cnt = 0
@@ -108,10 +117,36 @@ class manager_object:
 
         for thread in threads:
             threads[thread].join()
-        # for object in object_index:
-            # threads[object].join()
-
         return objects
+
+    def get_customers(self):
+        """Get all customers."""
+        return self.get_objects(self.index_customers())
+
+    def get_sales_invoices(self):
+        """Get all Sales Invoices."""
+        return self.get_objects(self.index_sales_invoices())
+
+    def get_tracking_codes(self):
+        """Get all the tracking codes."""
+        return self.get_objects(self.index_tracking_codes())
+
+    def get_cash_accounts(self):
+        """Get all cash accounts."""
+        return self.get_objects(self.index_cash_accounts())
+
+    def get_profit_loss_accounts(self):
+        """Get all accounts in the profit and loss statement."""
+        return self.get_objects(self.index_profit_loss_account())
+
+    def get_inventory(self):
+        """Get all the inventory Items."""
+        return self.get_objects(self.index_inventory())
+
+    def get_sales_inventory_items(self):
+        """Get all the sales inventory Items."""
+        return self.get_objects(self.index_sales_inventory_items())
+
 
 # Below, all the PUT commands for the server, to update objects
     def put_object(self, data, object_path):
@@ -120,19 +155,46 @@ class manager_object:
         pass
 
 # Below, all the POST commands for the server, to create objects
-    def post_object(self, data, collection):
+    def post(self, data, collection):
         """Post an object at that specific path."""
-        r = self.post_encodedURL(json.dump(data), self.index(collection))
+        print("Posting: ", data, "to: " + self.collections[collection])
+        r = self.session.post(self.root_url + self.collections[collection],
+                              data=json.dumps(data))
 
-        if '201' not in r.status_code:
+        if r.status_code != 201:
             print('Something went wrong', r.status_code)
-            print collection
-            print data
+            print(collection)
+            print(data)
 
         return r
 
-    def post_SalesInvoice(self, data):
+    def post_receipt(self, date, debit_account, payer,
+                     bank_clear_status, lines):
+        """POST to the Receipt collection."""
+        data = dict()
+        data['Date'] = date
+        data['DebitAccount'] = debit_account
+        data['Lines'] = lines
+        data['Payer'] = payer
+        data['BankClearStatus'] = bank_clear_status
+        return self.post_object(data, 'Receipt')
+
+    def post_customer(self, name, email):
+        """POST to the Customer collection."""
+        data = dict()
+        data['Name'] = name
+        data['Email'] = email
+        data['StartingBalanceType'] = 'Credit'
+        return self.post_object(data, 'Customer')
+
+    def post_sales_invoice(self, issue_date, ref, to, lines):
         """Post to the SalesInvoice collection."""
+        data = dict()
+        data['IssueDate'] = issue_date
+        data['Reference'] = ref
+        data['To'] = to
+        data['Lines'] = lines
+        data['AmountsIncludeTax'] = 'true'
         return self.post_object(data, 'SalesInvoice')
 
 # Below, all the DEL commands for the server, to delete objects
